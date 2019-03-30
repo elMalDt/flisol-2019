36b426e190694089c54ecf891138b1bc4f1c990b
elMalDto <miguemix55@hotmail.com>
2019-03-30 15:05:53 -0600

    pagina mis conferencias despues de registrarse

---
+++ views/mis-conferencias.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mis conferencias</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+  </head>
+  <body>
+    <div class="contain-conf-begin">
+      <div class="titulo">
+        <h1>Mis conferencias <br> por asistir: </h1>
+      </div>
+
+      <p class="begin-conf">Bienvenido, aun no tienes <br> conferencias por asistir.</p>
+
+      <input type="submit" name="" value="Añadir" class="btn-conferencias">
+    </div>
+
+  </body>
+</html>
