cec1329cecc48fc18b3bcda644ae0d7830901baf
elMalDto <miguemix55@hotmail.com>
2019-03-30 17:05:59 -0600

    pagina de conferencias por asistir en proceso

---
+++ views/add.view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Añadir conferencias</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+  </head>
+  <body>
+    <h1 class="titulo add-conf-titu">Añade las conferencias <br> a las que asistiras. </h1>
+    <div class="contain-cards">
+      <div class="tarjeta-add-conf">
+
+        <input type="checkbox" id="fruit1" name="fruit-1" value="Apple">
+        <label for="fruit1"></label>
+
+        <img src="../img/tarjetas-conf/ventas.jpg" alt="">
+        <h2>
+          Inteligencia Artificial
+        </h2>
+        <p>
+          Por: Autor Apellido Apellido
+        </p>
+        <p>
+          Hora: hh/mm
+        </p>
+        <p>
+          Lugar: Auditorio I
+        </p>
+      </div>
+
+      <div class="tarjeta-add-conf">
+        <img src="../img/tarjetas-conf/linux.jpg" alt="">
+        <h2>
+          Inteligencia Artificial
+        </h2>
+        <p>
+          Por: Autor Apellido Apellido
+        </p>
+        <p>
+          Hora: hh/mm
+        </p>
+        <p>
+          Lugar: Auditorio I
+        </p>
+      </div>
+
+      <div class="tarjeta-add-conf">
+        <img src="../img/tarjetas-conf/gitCr.png" alt="">
+        <h2>
+          Inteligencia Artificial
+        </h2>
+        <p>
+          Por: Autor Apellido Apellido
+        </p>
+        <p>
+          Hora: hh/mm
+        </p>
+        <p>
+          Lugar: Auditorio I
+        </p>
+      </div>
+
+
+    </div>
+    <div class="button-cont">
+      <button type="button" name="button" class="btn-conferencias">Guardar</button>
+      <button type="button" name="button" class="btn-conferencias">Consultar</button>
+    </div>
+  </body>
+</html>
