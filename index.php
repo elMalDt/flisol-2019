55aab2cd547ba692634cbd1ffdd51069aec92888
elMalDto <miguemix55@hotmail.com>
2019-04-09 22:41:06 -0500

    agregue pequeñas animaciones

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Lato|Roboto|Share+Tech" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estilosss.css">
     <script type="text/javascript" src="js/main.js">
 
     </script>
@@ -674,8 +674,6 @@
           </div>
         </div>
       </div>
-
-
 
       <div class="tab">
         <input id="tab-six" type="checkbox" name="tabs">
